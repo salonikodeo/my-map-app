1390784bb89237f8382425047e058a332c15c1ef
Saloni Gupta <saloni08g@gmail.com>
2018-11-18 01:26:34 +0530

    Add markers

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component {
+  state = {
+    venues: []
+  }
   componentDidMount() {
     this.getPlaces()
     this.loadMap()
@@ -18,12 +21,15 @@ class App extends Component {
     const parameters = {
       client_id : "GZW23AP4MZ5N5TPYFXWICJMZDGCQVHKMOXLAFLNCOQUZO3PA",
       client_secret : "CWCZRBRASQDLLMMVO3NKYQ5LLBPX1AQGKNMINMUN1RM0DYVB",
-      query : "food",
-      near : "Bhopal"
+      query : "foods",
+      near : "Bhopal, India",
+      v : "20182507"
     }
     axios.get(endPoint + new URLSearchParams(parameters))
     .then(response => {
-      console.log(response);
+      this.setState({
+        venues : response.data.response.groups[0].items
+      }, this.loadMap())
     })
     .catch(error => {
       console.log("ERROR! " + error);
@@ -32,9 +38,17 @@ class App extends Component {
 
   initMap = () => {
     let map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
+      center: {lat: 23.259933, lng: 77.412615},
       zoom: 8
     });
+    this.state.venues.map(ven => {
+      var marker = new window.google.maps.Marker({
+      position : {lat: ven.venue.location.lat, lng: ven.venue.location.lng},
+      map : map,
+      title : ven.venue.name
+      });
+    })
+  
   }
 
   render() {
